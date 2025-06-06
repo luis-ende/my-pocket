17d2e1e174bdcd3375a00f8537f8dad7b9c79a95
Jose Luis Salazar <lsalazar@gmx.de>
2025-06-06 14:17:37 -0600

    add DashboardController and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('dashboard/section/tags', [DashboardController::class, 'loadTags'])->name('dashboard.tags');
+    Route::get('dashboard/section/bookmarks', [DashboardController::class, 'loadBookmarksByTags'])->name('dashboard.bookmarks');
 });
 
 require __DIR__.'/settings.php';
