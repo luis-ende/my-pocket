f7607cfd3567bb6bae17bedf9d7697c0a54d344c
Jose Luis Salazar <lsalazar@gmx.de>
2025-08-01 12:31:29 -0600

    fix sidebar active button not being highligted due to mismatch between url and item path

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -4,13 +4,18 @@ import { Link, usePage } from '@inertiajs/react';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
+
+    const isSidebarMenuButtonActive = (href: string) => {
+        return page.url.split('?')[0] === href;
+    }
+
     return (
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel>Sections</SidebarGroupLabel>
             <SidebarMenu>
                 {items.map((item) => (
                     <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild isActive={item.href === page.url} tooltip={{ children: item.title }}>
+                        <SidebarMenuButton asChild isActive={isSidebarMenuButtonActive(item.href)} tooltip={{ children: item.title }} className={isSidebarMenuButtonActive(item.href) ? 'border' : ''}>
                             <Link href={item.href} prefetch>
                                 {item.icon && <item.icon />}
                                 <span>{item.title}</span>
