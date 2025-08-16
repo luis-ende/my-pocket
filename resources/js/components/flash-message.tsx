1810e9bc15450780f544519ee0fe21c1d32d151a
Jose Luis Salazar <lsalazar@gmx.de>
2025-08-16 17:28:11 -0600

    extended flash message to display error messages

--- resources/js/components/flash-message.tsx
+++ resources/js/components/flash-message.tsx
@@ -1,23 +1,30 @@
 import { usePage } from '@inertiajs/react';
-import { CheckCircle } from 'lucide-react';
+import { CheckCircle, CircleX  } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 export default function FlashMessage() {
     const { flash } = usePage().props;
     const [message, setMessage] = useState(null);
+    const [messageType, setMessageType] = useState('');
 
     useEffect(() => {
         setMessage(null);
         if (flash?.success) {
+            setMessageType('Success');
             setMessage(flash.success);
+        }
+
+        if (flash?.error) {
+            setMessageType('Error');
+            setMessage(flash.error);
         }
     }, [flash]);
 
     if (!message) return null;
 
-    return toast('Success', {
-        icon: <CheckCircle className="h-6 w-6 text-green-500" />,
-        description: flash?.success,
+    return toast( `${messageType}`, {
+        icon: messageType === 'Success' ? <CheckCircle className="h-6 w-6 text-green-500" /> : <CircleX className="h-6 w-6 text-red-500" />,
+        description: message,
         onAutoClose: () => setMessage(null),
         action: {
             label: 'Close',
